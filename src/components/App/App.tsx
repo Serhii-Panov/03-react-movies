46f9dbf33adc230527aaf41b156dd53093a48f32
Serhii Panov <suboxoneforhelp@gmail.com>
2025-12-07 23:09:44 +0200

    loader

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,25 +4,31 @@ import SearchBar from "../SearchBar/SearchBar";
 import MovieGrid from "../MovieGrid/MovieGrid";
 import { Toaster } from "react-hot-toast";
 import toast from "react-hot-toast";
+import Loader from "../Loader/Loader";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isloading, setIsLoading] = useState(false);
 
   const handleSearch = (query: string) => {
     const params = {
       params: { query },
       headers: { Authorization: "", Accept: "application/json" },
     };
+    setIsLoading(true);
     fetchMovies(params)
       .then((res) => {
         if (res.data.results.length === 0) {
+          setIsLoading(false);
           toast.error("No movies found for your request");
           setMovies([]);
         } else {
+          setIsLoading(false);
           setMovies(res.data.results);
         }
       })
       .catch((err) => {
+        setIsLoading(false);
         console.log(err);
         toast.error("There was an error, please try again...");
       });
@@ -32,6 +38,7 @@ function App() {
     <div>
       <Toaster />
       <SearchBar onSubmit={handleSearch} />
+      {isloading && <Loader />}
       <MovieGrid movies={movies} onSelect={(movieId) => {}} />
     </div>
   );
